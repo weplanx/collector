46558cff473dd2411dcdf632e18642a822714002
kain <zhangtqx@vip.qq.com>
2020-09-08 09:42:41 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # elastic-collector
 
 Provides to collect data from the queue and write it to elasticsearch
+
+![guide](guide.svg)
--- app/app.go
+++ app/app.go
@@ -1,1 +1,55 @@
 package app
+
+import (
+	"elastic-collector/app/controller"
+	"elastic-collector/app/manage"
+	"elastic-collector/app/mq"
+	"elastic-collector/app/types"
+	pb "elastic-collector/router"
+	"google.golang.org/grpc"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
+)
+
+type App struct {
+	option *types.Config
+}
+
+func New(config types.Config) *App {
+	app := new(App)
+	app.option = &config
+	return app
+}
+
+func (app *App) Start() (err error) {
+	// Turn on debugging
+	if app.option.Debug {
+		go func() {
+			http.ListenAndServe(":6060", nil)
+		}()
+	}
+	// Start microservice
+	listen, err := net.Listen("tcp", app.option.Listen)
+	if err != nil {
+		return
+	}
+	server := grpc.NewServer()
+	mqlib, err := mq.NewMessageQueue(app.option.Mq)
+	if err != nil {
+		return
+	}
+	manager, err := manage.NewElasticManager(
+		app.option.Elastic,
+		mqlib,
+	)
+	if err != nil {
+		return
+	}
+	pb.RegisterRouterServer(
+		server,
+		controller.New(manager),
+	)
+	server.Serve(listen)
+	return
+}
